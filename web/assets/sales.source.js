082539eb72f6b3bb407921a2868600313c993069
Евгений Деев <deevgen96@gmail.com>
2017-07-05 22:06:23 +0500

    Тщетные попытки сделать что-нибудь

--- web/assets/sales.source.js
+++ web/assets/sales.source.js
@@ -1,3 +1,33 @@
 /**
  * Created by 111 on 04.07.2017.
  */
+window.onload = function(){
+    var sls = new Vue({
+        el: '#sls',
+        delimiters: ['${', '}'],
+        data: {
+            sources: [],
+            source: '',
+            api_url: 'http://localhost/index.php'
+        },
+        methods: {
+            getSource: function () {
+                s = this;
+                console.log(s.api_url + '/api/v1/rest/source/');
+                $.get(s.api_url + '/api/v1/rest/source/').then(function (response) {
+                    console.log(s.api_url + '/api/v1/rest/source/');
+
+                    s.sources = response;
+                    console.log(s.sources);
+                }).catch(function(){
+                    console.log('Ошибка запроса данных');
+                });
+
+            }
+        },
+        started: function(){
+            this.getSource();
+        }
+    })
+
+}
